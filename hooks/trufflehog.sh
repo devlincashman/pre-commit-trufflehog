b6b03e6f20455a3a4e4481bb91f1023cdf08f7c4
Devlin Cashman <dcashman@pagerduty.com>
2022-04-21 14:10:39 -0400

    add path logic

--- hooks/trufflehog.sh
+++ hooks/trufflehog.sh
@@ -8,11 +8,21 @@ set -e
 original_path=$PATH
 export PATH=$PATH:/usr/local/bin
 
-# Store and return last failure from fmt so this can validate every directory passed before exiting
+# Since trufflehog only supports directory based scanning we want to scan the minimal number of times by removing duplicate directories.
+# This means we'll still end up scanning some files not included in the commit but this is the best we can do for now.
 FOUND_CREDENTIALS=0
+PATHS=()
 
+# strip out filenames and just get the paths
 for file in "$@"; do
-  trufflehog filesystem --directory="$file" || FOUND_CREDENTIALS=$?
+  PATHS+=("${file%/*}")
+done
+
+# return a new array of only unique paths https://stackoverflow.com/questions/13648410/how-can-i-get-unique-values-from-an-array-in-bash
+UNIQ_PATHS=" " read -r -a PATHS <<< "$(echo "${PATHS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')"
+
+for uniq_path in $UNIQ_PATHS; do
+  trufflehog filesystem --no-update --directory="$uniq_path" || FOUND_CREDENTIALS=$?
 done
 
 # reset path to the original value
